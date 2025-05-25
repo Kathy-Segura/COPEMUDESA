3103506f74378a548d7f91a4e0dd3102901944df
katherine <katherinesofia719@gmail.com>
2025-05-24 22:37:23 -0600

    Modificacion en el layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -14,7 +15,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* ✅ Script de Metricool */}
+        <Script id="metricool" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].q=m[i].q||[]).push(arguments)};
+              m[i].l=1*new Date();
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+              k.async=1;k.src=r;a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://tracker.metricool.com/resources/be.js", "metr");
+
+            metr('init', { apiKey: "365fcbdf8c1325ae7bc34ee1ab3e8fd3" });
+          `}
+        </Script>
+
+        {children}
+      </body>
     </html>
   )
 }
